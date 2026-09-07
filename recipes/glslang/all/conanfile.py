f91d9464204c757c3e90c26a0159498e02d0e4ba
Robbe Beernaert <robbebeernaert@hotmail.be>
2026-09-07 16:19:16 +0200

    glslang: change max cmake version to <5 instead of <4

--- recipes/glslang/all/conanfile.py
+++ recipes/glslang/all/conanfile.py
@@ -79,7 +79,7 @@ class GlslangConan(ConanFile):
 
     def build_requirements(self):
         if Version(self.version) >= "1.3.261":
-            self.tool_requires("cmake/[>=3.17.2 <4]")
+            self.tool_requires("cmake/[>=3.17.2 <5]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
